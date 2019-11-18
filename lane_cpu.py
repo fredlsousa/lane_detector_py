8755127628e282889de5807cc12040f0bd8a0c97
MacFred <frederico.sousa@aluno.ufop.edu.br>
2019-11-17 21:53:11 -0300

    draw lines

--- lane_cpu.py
+++ lane_cpu.py
@@ -3,9 +3,9 @@ import numpy as np
 
 
 def bird_view(crop_frame):
-    image_h, image_w, _ = crop_frame
-    src = np.float32([[0, image_h], [1207, image_h], [0, 0], [image_w, 0]])
-    dst = np.float32([[568, image_h], [711, image_h], [0, 0], [image_w, 0]])
+    image_h, image_w = crop_frame.shape
+    src = np.float32([[0, image_h], [1000, image_h], [0, 0], [image_w, 0]])     # 1207
+    dst = np.float32([[520, image_h], [780, image_h], [0, 0], [image_w, 0]])    # 568, 711, (420, 720)
     M = cv2.getPerspectiveTransform(src, dst)
     bird_img = cv2.warpPerspective(crop_frame, M, (image_w, image_h))
     return bird_img
@@ -29,16 +29,39 @@ def cam_module():
         normalized_img = median
         cv2.normalize(median, normalized_img, 0, 255, cv2.NORM_MINMAX)
         warp = bird_view(normalized_img)
-        cv2.imshow("warp", warp)
+        # cv2.imshow("warp", warp)
         # cv2.imshow("norm", normalized_img)
         low_threshold = 50
         high_threshold = 150
         canny = cv2.Canny(warp, low_threshold, high_threshold)
         cv2.imshow("filtered", canny)
+        line_detect = cv2.HoughLines(canny, 1, np.pi/160, 200)
 
+        # for r, theta in line_detect[0]:
+        #     a = np.cos(theta)
+        #     b = np.sin(theta)
+        #     x0 = a*r
+        #     y0 = b*r
+        #     x1 = int(x0 + 1000*(-b))
+        #     y1 = int(y0 + 1000*(a))
+        #     x2 = int(x0 - 1000*(-b))
+        #     y2 = int(y0 - 1000*(a))
+        #     cv2.line(crop_img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+        if line_detect is not None:
+            for i in range(0, len(line_detect)):
+                rho = line_detect[i][0][0]
+                theta = line_detect[i][0][1]
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = a * rho
+                y0 = b * rho
+                pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
+                pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
+                cv2.line(crop_img, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
 
         # out.write(frame)
-
+        cv2.imshow("final", crop_img)
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
